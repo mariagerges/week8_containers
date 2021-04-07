d43bc545e1c22b4de0993e008a53480fefb57a65
mariagerges <Maria.Gerges.23@cmc.edu>
2021-04-06 21:38:42 -0700

    done

--- containers/unicode.py
+++ containers/unicode.py
@@ -6,13 +6,11 @@ class NormalizedStr:
     By default, Python's str type stores any valid unicode string.
     This can result in unintuitive behavior.
     For example:
-
     >>> 'César' in 'César Chávez'
     True
     >>> 'César' in 'César Chávez'
     False
-
-    The two strings to the right of the in keyword above are equal *semantically*,
+    The two strings to the right of the in keyword above are equal
     but not equal *representationally*.
     In particular, the first is in NFC form, and the second is in NFD form.
     The purpose of this class is to automatically normalize our strings for us,
@@ -20,19 +18,27 @@ class NormalizedStr:
     '''
 
     def __init__(self, text, normal_form='NFC'):
-        pass
+        self.text = unicodedata.normalize(normal_form, text)
+        self.normal_form = normal_form
 
     def __repr__(self):
         '''
-        The string returned by the __repr__ function should be valid python code
-        that can be substituted directly into the python interpreter to reproduce an equivalent object.
+        The string returned by the __repr__ function should be valid python
+        that can be substituted directly into the python interprete
         '''
+
+        text = str(self.text)
+        form = str(self.normal_form)
+
+        return "NormalizedStr('" + text + "', '" + form + "')"
 
     def __str__(self):
         '''
         This functions converts the NormalizedStr into a regular string object.
-        The output is similar, but not exactly the same, as the __repr__ function.
+        The output is similar, but not exactly the same, as the __repr__
         '''
+
+        return str(self.text)
 
     def __len__(self):
         '''
@@ -40,14 +46,20 @@ class NormalizedStr:
         The expression `len(a)` desugars to a.__len__().
         '''
 
+        return len(self.text)
+
     def __contains__(self, substr):
         '''
         Returns true if the `substr` variable is contained within `self`.
         The expression `a in b` desugars to `b.__contains__(a)`.
-
-        HINT:
-        You should normalize the `substr` variable to ensure that the comparison is done semantically and not syntactically.
         '''
+
+        normal_substr = unicodedata.normalize(self.normal_form, substr)
+
+        if normal_substr in self.text:
+            return True
+
+        return False
 
     def __getitem__(self, index):
         '''
@@ -55,30 +67,60 @@ class NormalizedStr:
         The expression `a[b]` desugars to `a.__getitem__(b)`.
         '''
 
+        return self.text[index]
+
     def lower(self):
         '''
         Returns a copy in the same normalized form, but lower case.
         '''
+
+        return str(self.text).lower()
 
     def upper(self):
         '''
         Returns a copy in the same normalized form, but upper case.
         '''
 
+        return str(self.text).upper()
+
     def __add__(self, b):
         '''
         Returns a copy of `self` with `b` appended to the end.
         The expression `a + b` gets desugared into `a.__add__(b)`.
-
         HINT:
-        The addition of two normalized strings is not guaranteed to stay normalized.
-        Therefore, you must renormalize the strings after adding them together.
+        The addition of two normalized strings is not
+        guaranteed to stay normalized.
+        Therefore, you must renormalize the strings
+        after adding them together.
         '''
+
+        combined_string = self.text + str(b)
+        normal = unicodedata.normalize(self.normal_form, combined_string)
+
+        return NormalizedStr(normal)
 
     def __iter__(self):
         '''
         HINT:
-        Recall that the __iter__ method returns a class, which is the iterator object.
-        You'll need to define your own iterator class with the appropriate magic methods,
+        Recall that the __iter__ method returns a class,
+        which is the iterator object.
+        You'll need to define your own iterator class
+        with the appropriate magic methods,
         and return an instance of that class here.
         '''
+
+        return Iter(self.text)
+
+
+class Iter:
+    def __init__(self, text):
+        self.text = text
+        self.counter = 0
+
+    def __next__(self):
+        if len(self.text) <= self.counter:
+            raise StopIteration
+        else:
+            final = self.text[self.counter]
+            self.counter += 1
+            return final
